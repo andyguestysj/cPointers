a85184116a55900a2e0427745e0baeb574a7ad3a
andyguest <59604680+andyguestysj@users.noreply.github.com>
2021-10-19 09:00:10 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+  int var = 20;   
+
+  // Exercise tasks
+  // 1 Create a pointer variable that stores the address of var
+  // 2 Print out the address of var using &var
+  // 3 Print out the value stored in your pointer, ensure it is the same as the address from &var
+  // 4 Print out the value of var
+  // 5 Print out the value your pointer is pointing to using the * redirector
+  // 6 After your printfs change the value stored in var using var =  and output the four printfs from 2-6 again. 
+  // 7 Now change the value using your pointer and the * redirection. Again output the four printfs after to see what has changed.
+
+  return 0;
+}
